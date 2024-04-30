2c65e21831ae71c0c2947ec45d47131bb07ac0e4
George K <gkwambata@gmail.com>
2024-05-01 00:17:42 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      padding: '4rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
